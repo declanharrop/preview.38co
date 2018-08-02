972b06fb8b9e0b680ae444de1bc73d4d20e9db7e
Declan Harrop <dh@38co.io>
2018-08-02 13:04:42 +0100

    refresh

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import 'tachyons';
-import Navigation from './Components/Navigation';
-import Home from './Components/Home';
-import Services from './Components/Services';
-import About from './Components/About';
-import Blog from './Components/Blog';
-import Contact from './Components/Contact';
-import Err from './Components/Err';
-import Footer from './Components/Footer';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Navigation />
-          <Switch>
-            <Route path="/" component={Home} exact/>
-            <Route path="/Services" component={Services}/>
-            <Route path="/About" component={About}/>
-            <Route path="/Blog" component={Blog}/>
-            <Route path="/Contact" component={Contact}/>
-            <Route component={Err} />
-          </Switch>
-          <Footer />
-        </div>
-      </BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
