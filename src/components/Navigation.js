d91853666376d52fbfb91ba97188ab9a73a54d5b
Declan Harrop <dh@38co.io>
2018-08-04 10:23:16 +0100

    Navigation styles and layout complete

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,10 +5,14 @@ import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
 
 import HomeIcon from '@material-ui/icons/Home';
 import ServicesIcon from '@material-ui/icons/Code';
+import AboutIcon from '@material-ui/icons/AccountCircle';
+import BlogIcon from '@material-ui/icons/Book';
 import ContactIcon from '@material-ui/icons/LocalPhone';
+
 
 import './Navigation.css'
 
@@ -18,7 +22,7 @@ const styles = {
   },
 
   drawer: {
-    backgroundColor: 'green',
+    backgroundColor: 'grey',
   },
 
   menuButton: {
@@ -26,7 +30,11 @@ const styles = {
     color:'black',
     width: '40px',
     height: '40px',
-    margin: '20px'  
+    margin: '20px',
+  },
+
+  Logo: {
+    color: '#3F424C',
   }
 };
 
@@ -47,95 +55,97 @@ class Navigation extends React.Component {
       <div className='avenir'>
         <List className={classes.list}>
 
-          
+          <div className='pv3 w-100'>
+            <img className='centered' style={{height: '100px'}} src={require('../img/TECLogo.png')} alt='38co Logo' />
+          </div>
           <NavLink to='/'>
-            <div className='pv1'>
+            <div className='pv1 pt2'>
               <div className="mw7 center ph1-ns bg-white">
                 <div className="cf ph1-ns">
                   <div className="fl w-30 pa2 pl3">
                     <div className="pv1 h2">
-                    <HomeIcon/>
+                    <HomeIcon className={classes.Logo}/>
                     </div>
                   </div>
                   <div className="fl w-70 pa2">
                     <div className="pv1 h2 dt">
-                      <p className='dtc v-mid ph2'>Home</p>
+                      <p className='dtc v-mid ph2 menuItem'>Home</p>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </NavLink>
-
+          <Divider />
           <NavLink to='/services'>
             <div className='pv1'>
               <div className="mw7 center ph1-ns bg-white">
                 <div className="cf ph1-ns">
                   <div className="fl w-30 pa2 pl3">
                     <div className="pv1 h2">
-                    <ServicesIcon/>
+                    <ServicesIcon className={classes.Logo}/>
                     </div>
                   </div>
                   <div className="fl w-70 pa2">
                     <div className="pv1 h2 dt">
-                      <p className='dtc v-mid ph2'>Services</p>
+                      <p className='dtc v-mid ph2 menuItem'>Services</p>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </NavLink>
-
+          <Divider />
           <NavLink to='/about'>
             <div className='pv1'>
               <div className="mw7 center ph1-ns bg-white">
                 <div className="cf ph1-ns">
                   <div className="fl w-30 pa2 pl3">
                     <div className="pv1 h2">
-                    <ContactIcon/>
+                    <AboutIcon className={classes.Logo}/>
                     </div>
                   </div>
                   <div className="fl w-70 pa2">
                     <div className="pv1 h2 dt">
-                      <p className='dtc v-mid ph2'>About</p>
+                      <p className='dtc v-mid ph2 menuItem'>About</p>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </NavLink>
-
+          <Divider />
           <NavLink to='/blog'>
             <div className='pv1'>
               <div className="mw7 center ph1-ns bg-white">
                 <div className="cf ph1-ns">
                   <div className="fl w-30 pa2 pl3">
                     <div className="pv1 h2">
-                    <ContactIcon/>
+                    <BlogIcon className={classes.Logo}/>
                     </div>
                   </div>
                   <div className="fl w-70 pa2">
                     <div className="pv1 h2 dt">
-                      <p className='dtc v-mid ph2'>Blog</p>
+                      <p className='dtc v-mid ph2 menuItem'>Blog</p>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </NavLink>
-
+          <Divider />
           <NavLink to='/contact'>
-          <div className='pv1'>
+            <div className='pv1'>
               <div className="mw7 center ph1-ns bg-white">
                 <div className="cf ph1-ns">
                   <div className="fl w-30 pa2 pl3">
                     <div className="pv1 h2">
-                    <ContactIcon/>
+                    <ContactIcon className={classes.Logo}/>
                     </div>
                   </div>
                   <div className="fl w-70 pa2">
                     <div className="pv1 h2 dt">
-                      <p className='dtc v-mid ph2'>Contact</p>
+                      <p className='dtc v-mid ph2 menuItem'>Contact</p>
                     </div>
                   </div>
                 </div>
@@ -151,7 +161,6 @@ class Navigation extends React.Component {
         <Button className={classes.menuButton} onMouseEnter={classes.linkHover} onClick={this.toggleDrawer('left', true)}>M</Button>
         <Drawer open={this.state.left} onClose={this.toggleDrawer('left', false)}>
           <div
-            className={classes.drawer}
             tabIndex={0}
             role="button"
             onClick={this.toggleDrawer('left', false)}
